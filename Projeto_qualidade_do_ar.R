e97843fe8e738ca3a4686dd4fffc904b30da97d2
Danna <Danna.ses9@gmail.com>
2023-04-30 04:17:33 -0300

    Análise exploratória com o comando Sumarry e análise de gráfico simples com o comando Plot

---
+++ Projeto_qualidade_do_ar.R
@@ -0,0 +1,30 @@
+# PROJETO DO CURSO: "O que afeta a qualidade do ar e como é afetado?"
+# Autor: Danna SES
+
+
+# Install packages Ecdat - Para extrair informações do banco de dados
+# install.packages(Ecdat) 
+
+library(Ecdat) # Carregando o pacote
+
+data("Airq")   # Buscando o banco de dados disponível no pacote
+
+names(Airq)    # Exibe o nome das variáveis
+
+# VARIÁVEL RESPOSTA
+# Airq: Indice da qualidade do ar (< melhor)
+
+# VARIAVEIS EXPLICATIVAS (explica a variação da variável RESPOSTA)
+# Vala: Valor das empresas nas cidades (em milhares de dólares)
+# rain: Quantidade de chuva em polegadas
+# Coas: Posição costeira da cidade (Y/N)
+# Dens: Densidade populacional (milha quadrada)
+# Medi: Renda média per capita (dólares)
+
+# Análise descritiva ou exploratória
+
+summary(Airq) # Sumário das variáveis 
+# As variáveis podem ser contínuas ou categóricas 
+
+# Gerando um gráfico simples
+plot(airq~vala, data=Airq) # Variável resposta EM FUNÇÃO DE variável explicativa, no banco de dados
